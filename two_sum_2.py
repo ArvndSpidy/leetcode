e3fbafb775e5da5b712825542be89eddb37bd434
Aravind Chilakamarri <iamaravindkumarme@gmail.com>
2023-03-02 18:25:44 -0600

    feat: add LC157-two-sum-II

---
+++ two_sum_2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum2(self, numbers: list[int], target: int) -> list[int]:
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            if numbers[l] + numbers[r] < target:
+                l += 1
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                return [l + 1, r + 1]
+        return []
+    
+solution = Solution()
+print(solution.twoSum2([2,7,11,15], 9))
