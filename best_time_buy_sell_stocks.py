df155c712cf2dfd4004871eeec1ffe1a25d5f17e
Aravind Chilakamarri <iamaravindkumarme@gmail.com>
2023-03-03 11:46:00 -0600

    feat: Update Rev1 LC121-Best-time-to-sell stocks

--- best_time_buy_sell_stocks.py
+++ best_time_buy_sell_stocks.py
@@ -1,3 +1,17 @@
+"""
+Revision number: 1
+
+Description:
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Notes:
+Add two pointers for indices left:0 and right:1. Now check if the left index value is less than right index value.
+check the difference between 2 values and update the profit. After that move the right index to check the profit again.
+If at any point, left index value is greater than right index value then update the left index value with right index value and increment the right index, as we know that if right value is less than left value then that must be the smallest number. 
+"""
+
 class Solution:
     def maxProfit(self, prices:list[int]) -> int:
         l,r = 0,1
